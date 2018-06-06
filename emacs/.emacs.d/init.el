c5a8ae45eb9784545ff4b026317b399f4604963c
Sohom Bhattacharjee <soham.bhattacharjee@gmail.com>
2018-06-06 08:15:47 +0530

    added anacomda mode and anaconda paths

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,11 +2,13 @@
 (require 'package) 
 ;; set the repos from where to get the packages
 (setq package-enable-at-startup nil)
+
+(package-initialize)
 (setq package-archives '(("melpa-stable" . "http://stable.melpa.org/packages/")
 				("org" . "http://orgmode.org/elpa/")
 				("gnu" . "http://elpa.gnu.org/packages/")))
-
-(package-initialize)
+(package-refresh-contents)
 
 ;; this line loads the emacs config from the org mode file.
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
