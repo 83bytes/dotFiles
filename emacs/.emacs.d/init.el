23cea2ff5314b747de2aa10bd480f2684c040332
Sohom Bhattacharjee <sohom.b@peopleinteractive.in>
2020-07-20 23:26:59 +0530

    emacs for smooth scrolling and better capture templates for detailed tasks

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -12,20 +12,36 @@
 ;; this line loads the emacs config from the org mode file.
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
+;; Add things to be moved to cofig.org here
 
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (python . t)
+   (shell . t)
+   ))
 
-
-
-;; (global-set-key (kbd "C- C-f") #'helm-do-ag-project-root)
-
-;; (require 'deft)
-
-;; (setq deft-extensions '("txt" "tex" "org"))
-
-;; (setq deft-directory "~/.deft/")
-
-
-;; (global-set-key (kbd "C-f") 'deft)
 
 ;; Add things here. Move them to config later on
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-replace-threshold 50)
+ '(anzu-replace-to-string-separator " => ")
+ '(anzu-search-threshold 1000)
+ '(helm-ag-base-command "rg --no-heading")
+ '(helm-source-names-using-follow (quote ("Grep")))
+ '(org-refile-targets (quote (("todoArchive.org" :maxlevel . 1))))
+ '(package-selected-packages
+   (quote
+    (ranger zenburn-theme yaml-mode xpm wgrep-helm use-package tablist switch-window start-menu solarized-theme smooth-scrolling sml-mode smart-mode-line-powerline-theme restclient racket-mode pythonic php-mode org multi-web-mode markdown-mode magit linum-relative ledger-mode hideshow-org helm-swoop helm-projectile helm-ag go-mode go-autocomplete exwm exec-path-from-shell evil-smartparens emms elpy dumb-jump dmenu deft company-terraform color-theme beacon anzu ac-geiser))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
