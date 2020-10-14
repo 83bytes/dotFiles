526cd3b6816dbe7fbde9f5e68c05b0a0277cf0ef
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2020-10-15 03:14:43 +0530

    get stats in the end of the run and per file progress while transferring.
    also ditch compression for local transfer. Speeds up tx rate by almost 50%

--- scripts/backup_media.sh
+++ scripts/backup_media.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 
 
 function dry_run {
-     rsync  -rtzh --update --info=stats2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/  -n
-     rsync  -rtzh --update --info=stats2 --progress  /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/  -n
+     rsync  -rth --update --info=stats2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/  -n
+     rsync  -rth --update --info=stats2 --progress  /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/  -n
 }
 
 function do_it {
-     rsync  -rtzh --update --info=stats2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/ 
-     rsync  -rtzh --update --info=progress2,stats2 --progress /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/ 
+     rsync  -rth --update --info=stats2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/
+     rsync  -rth --update --info=stats2 --progress /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/
 }
 
 $1
