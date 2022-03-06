a8ae88fb5cfcbd5c0602112c154bc439fd8649da
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2022-03-06 13:14:01 +0530

    scripts

--- scripts/backup_media.sh
+++ scripts/backup_media.sh
@@ -2,15 +2,20 @@
 
 set -euo pipefail
 
+# r recursive
+# a archive mode (preserves a bunch of things)
+# t time (covered in "a" already)
+# h human readable
+
 
 function dry_run {
-     rsync  -rth --update --info=stats2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/  -n
-     rsync  -rth --update --info=stats2 --progress  /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/  -n
+     rsync  -rthai --update --info=progress2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/  -n
+     rsync  -rthai --update --info=progress2 --progress  /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/  -n
 }
 
 function do_it {
-     rsync  -rth --update --info=stats2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/
-     rsync  -rth --update --info=stats2 --progress /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/
+     rsync  -rthai --update --info=progress2 --progress /mnt/media/stuff1/   /media/PEOPLE\\sohomb/stuff1/
+     rsync  -rthai --update --info=progress2 --progress /mnt/media/stuff2/   /media/PEOPLE\\sohomb/stuff2/
 }
 
 $1
